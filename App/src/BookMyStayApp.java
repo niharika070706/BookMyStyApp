0297577dce97d9b5845ef6f9aad593a5e9979a29
niharika <niharikalodha123@gmail.com>
2026-03-11 15:02:50 +0530

    Niharika added dev

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,20 +1,13 @@
 public class BookMyStayApp {
+    public static void main(String[] args) {
 
-        /**
-         * Main method - entry point of the application
-         * JVM starts execution from this method
-         *
-         * @param args command line arguments
-         */
-        public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
 
-            System.out.println("=================================");
-            System.out.println(" Welcome to Book My Stay ");
-            System.out.println(" Hotel Booking System v1.0 ");
-            System.out.println("=================================");
-
-            System.out.println("Application started successfully.");
-            System.out.println("Thank you for using Book My Stay!");
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay!");
         }
     }
 }
